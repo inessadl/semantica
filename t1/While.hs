7df2e696d2d8fed38d4da66259f84b1198509292
Inessa Luerce <idluerce@inf.ufpel.edu.br>
2016-07-11 18:19:20 -0300

    implementação finalizada

--- t1/While.hs
+++ t1/While.hs
@@ -26,15 +26,17 @@ data BExp = TRUE
       | And BExp BExp
       | Or BExp BExp
       | Ig AExp AExp
+      | Leq AExp AExp
     deriving(Show)
 
 data CExp = While BExp CExp
       | If BExp CExp CExp
       | Seq CExp CExp
       | Atrib AExp AExp
-      | Do CExp While BExp
-      | Repeat CExp Until BExp
-      | Loop CExp BExp
+      | DoWhile CExp BExp
+      | RepeatUntil CExp BExp
+      | Loop Int CExp
+      | DuplaAtrib AExp AExp AExp AExp
       | Skip
     deriving(Show)
 
@@ -69,8 +71,9 @@ bbigStep (And e1 e2, s) = let (b1, s1) = bbigStep(e1,s);
 bbigStep (Or e1 e2, s) = let (b1, s1) = bbigStep(e1, s);
 							(b2, s2) = bbigStep(e2, s)
 							in (b1 || b2, s)
-
-
+bbigStep (Leq e1 e2, s) = let (n1,s1) = abigStep(e1,s);
+							(n2,s2) = abigStep(e2,s)
+							in (n1 <= n2, s)
 -- Comandos
 cbigStep :: (CExp,Estado) -> (CExp,Estado)
 cbigStep (Skip,s) = (Skip,s)
@@ -80,24 +83,6 @@ cbigStep (While b c, s) = let (b1,s1) = bbigStep (b,s)
 										(_,s3) = cbigStep (While b c,s2)
 										in (Skip,s3)
 								False -> (Skip,s)
-cbigStep(Do c While b, s) = let (_, s1) = cbigStep(c, s);
-							(b, s2) = bbigStep(b, s1)
-							in case b of
-								True -> let (c, s2) = cbigStep(Do c While b, s1)
-										in (Skip, s2)
-								False -> cbigStep(Skip, s1)
-cbigStep(Repeat c Until b) = let (_,s1) = cbigStep(c,s);
-							(b, s2) = bbigStep(b, s1)
-							in case b of
-								True -> cbigStep(Skip, s1)
-								False -> let (c, s2) = cbigStep(Repeat c Until b, s1)
-										in (Skip, s2)
-cbigStep(Loop e c) = let (_,s1) = cbigStep(e, s);
-							(c, s2) = bbigStep(c, s1)
-							in case c of
-								True -> let (e, s2) = cbigStep(Loop e c, s1)
-										in (Skip, s2)
-								False -> cbigStep(Skip, s1)
 cbigStep (If b c1 c2,s) = let (b1, s1) = bbigStep(b, s)
 							in case b1 of
 								True -> let (_,s2) = cbigStep(c1, s1);
@@ -107,11 +92,23 @@ cbigStep (If b c1 c2,s) = let (b1, s1) = bbigStep(b, s)
 cbigStep (Seq c1 c2,s)  = let (_, s1) = cbigStep (c1, s);
 							(_, s2) = cbigStep (c2, s1)
 							in (Skip, s2)
-cbigStep (Atrib (Var x) e,s) = let (x,s1) = abigStep(e,s);
-							in (Skip, s1)
--- cbigStep(x, y := e1, e2) =
--- d) x, y := E1, E2
-
+-- retorna skip com a memória modificada
+cbigStep (Atrib (Var x) e,s) = let (e1,s1) = abigStep(e,s);
+							in (Skip, mudaVar s x e1)
+cbigStep(DoWhile c b, s) = let (_, s1) = cbigStep(c, s);
+							(b1, s2) = bbigStep(b, s1)
+							in case b1 of
+								True -> let (e1, s3) = cbigStep(DoWhile c b, s1) in (e1, s3)
+								False -> cbigStep(Skip, s1)
+-- cbigStep(Do c While b, s) = let (_, s1) = cbigStep(c, s);
+-- 							(b1, s1) = bbigStep(b, s1) in if b1 then cbigStep(Do c While b, s1) else cbigStep(Skip, s1)
+cbigStep(RepeatUntil c b, s) = let (_,s1) = cbigStep(c,s);
+							(b1, s2) = bbigStep(b, s1)
+							in case b1 of
+								True -> cbigStep(Skip, s1)
+								False -> let (e1, s3) = cbigStep(RepeatUntil c b, s1) in (e1, s3)
+cbigStep(Loop e c, s) = if (e > 0) then let (_,s1) = cbigStep(c, s) in cbigStep(Loop (e-1) c, s1) else cbigStep(Skip, s)
+cbigStep(DuplaAtrib x y e1 e2, s) = cbigStep(Seq (Atrib x e1)(Atrib y e2), s)
 
 
 
@@ -135,8 +132,13 @@ est3 :: Estado
 est3 = [("x", 2), ("y", 3)]
 
 
-b1 :: Estado
-b1 = [("x", 0), ("y", 0)]
+--
+estadoUnico :: Estado
+estadoUnico = [("x", 4)]
+
+estadoUnico' :: Estado
+estadoUnico' = [("x", 10)]
+
 
 -- Exemplos
 
@@ -156,19 +158,44 @@ exMul = Mul (Var "x") (Var "y")
 exAnd :: BExp
 exAnd = And TRUE TRUE
 
+-- Atribuição
+exAtrib :: CExp
+exAtrib = Atrib (Var "x") (Num 15)
 
---exAnd' :: BExpcbigStep :: (CExp,Estado) -> (CExp,Estado)
---cbigStep (Skip,s) = (Skip,s)
---cbigStep (While b c, s) = let (b1,s1) = bbigStep (b,s)
---							in case b1 of
---								True -> let (_,s2) = cbigStep (c,s);
---										(_,s3) = cbigStep (While b c,s2)
---										in (Skip,s3)
---								False -> (Skip,s)
-----cbigStep (If b c1 c2,s) =
---cbigStep (Seq c1 c2,s)  = let (_, s1) = cbigStep (c1, s);
---							  (_, s2) = cbigStep (c2, s1)
---							  in (Skip, s2)
+-- Incrementa
+exIncrementa :: CExp
+exIncrementa = Atrib (Var "x") (Som (Var "x")(Num 1))
+
+-- Incrementa
+exIncrementa2 :: CExp
+exIncrementa2 = Atrib (Var "x") (Som (Var "x")(Num 2))
+
+-- Less or equal
+exLeq :: BExp
+exLeq = Leq (Var "x")(Num 10)
+
+-- Loop e c
+exLoop :: CExp
+exLoop = Loop 5 exIncrementa2
+
+-- cbigStep(DoWhile exAtrib FALSE, estadoUnico)
+
+-- do While
+-- exDoWhile :: CExp
+-- exDoWhile = Do (Atrib (Var "x") (Num 15)) While (False, s1)
+--
+-- exAnd' :: BExpcbigStep :: (CExp,Estado) -> (CExp,Estado)
+-- cbigStep (Skip,s) = (Skip,s)
+-- cbigStep (While b c, s) = let (b1,s1) = bbigStep (b,s)
+-- 							in case b1 of
+-- 								True -> let (_,s2) = cbigStep (c,s);
+-- 										(_,s3) = cbigStep (While b c,s2)
+-- 										in (Skip,s3)
+-- 								False -> (Skip,s)
+-- --cbigStep (If b c1 c2,s) =
+-- cbigStep (Seq c1 c2,s)  = let (_, s1) = cbigStep (c1, s);
+-- 							  (_, s2) = cbigStep (c2, s1)
+-- 							  in (Skip, s2)
 --exAnd' =
 
 --teste1 :: BExp
